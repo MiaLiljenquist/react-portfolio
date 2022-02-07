498c282ad512cc0d7c6de622ccf5aa627239b6bf
mialil <mia.liljenquist@gmail.com>
2022-02-07 10:08:07 -0700

    changes

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
+import PortfolioItem from './portfolio-item';
+
 export default class PortfolioContainer extends Component {
+  Constructor() {
+    console.log("Portfolio container hes rendered");
+  }
+
+
+
+  PortfolioItem() {
+    const data = ["McDounles","Smiths","Ace Hardwere","City Hall"];
+
+    return data.map(item => {
+      return <PortfolioItem  title={item} />;
+    })
+  }
+
+
   render() {
       return(
          <div>
              <h2>Portfolio items go here...</h2>
+
+             {this.PortfolioItem()}
          </div> 
       );
   }
