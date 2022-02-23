2846695ecc7f92ee39288f14b231b2274d2815f1
mialil <mia.liljenquist@gmail.com>
2022-02-22 17:19:30 -0700

    changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,31 @@
-import React, { Component } from 'react';
-import moment from 'moment';
+import React, { Component } from "react";
+import moment from "moment";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
-import PortfolioContainer from './portfolio/portfolio-container';
-import NavigationContainer from './navigation/navigation-container';
+import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <NavigationContainer />
+      <div className="app">
+          <Router>
+            <div>
+              <NavigationContainer/>
+
+              <Switch>
+                <Route exact path="/" component = {Home} />
+                <Route exact path="/about-me" component = {About} />
+              </Switch>
+            </div>
+          </Router>
+
         <h1>Mia Liljenquist Portfolio</h1>
         <div>{moment().format('MMMM Do YYYYY, h:mm:ss a')}</div>
         <PortfolioContainer />
